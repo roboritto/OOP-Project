6ca4e373bf47560d1e671b50771db17e27ea901f
adlinaliya <nur_24003411@utp.edu.my>
2024-10-13 21:56:40 +0800

    Add files via upload

---
+++ TaskList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProgram
+{
+    public class TaskList : List<Task>
+    {
+        public List<Task> Tasks {  get; set; }
+        public void AddTask(Task task)
+        {
+            Tasks.Add(task);
+        }
+
+        public void RemoveTask(Task task)
+        {
+            Tasks.Remove(task);
+        }
+
+        public List<Task> GetTask()
+        {
+            return Tasks;
+        }
+
+        public Task FindTask(string taskName)
+        {
+            return Tasks.FirstOrDefault(t => t.TaskName == taskName);
+        }
+    }
+}
