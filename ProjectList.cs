993c444073275bf4e971ba7668532d83ea8be321
NURUL IMANUNNADHIRAH BINTI MOHD SHAFARUDDIN <nurul_24003464@utp.edu.my>
2024-10-14 18:49:48 +0800

    Add files via upload

---
+++ ProjectList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProgram
+{
+    public class ProjectList : List<Project>
+    {
+        public List<Project> Projects { get; set; }
+
+        public void AddProject(Project project)
+        {
+            Projects.Add(project);
+        }
+
+        public void RemoveProject(Project project)
+        {
+            Projects.Remove(project);
+        }
+
+        public List<Project> GetProjects()
+        {
+            return Projects;
+        }
+
+        public Project FindProject(string projectName)
+        {
+            return Projects.FirstOrDefault(p => p.Name == projectName);
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProgram
+{
+    public class Team
+    {
+        public string Name { get; set; }
+    }
+}
