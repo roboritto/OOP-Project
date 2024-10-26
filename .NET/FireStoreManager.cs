5b84d8462e5e4b18d02b5a8db61d7a724a7c8c26
adlinaliya <nur_24003411@utp.edu.my>
2024-10-26 11:39:34 +0800

    Add files via upload

---
+++ .NET/FireStoreManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+
+namespace ClassLibrary
+{
+    public class FireStoreManager
+    {
+        private const string FIREBASE_PROJID = "oop-web-43901";
+        private FirestoreDb _firestoreDb;
+        //public string Login {  get; set; }
+
+        public FireStoreManager(string projectId)
+        {
+            projectId = FIREBASE_PROJID;
+            _firestoreDb = FirestoreDb.Create(projectId);
+        }
+
+        /*public void initFirestore()
+        {
+            FirebaseApp.Create();
+            _firestoreDb = FirestoreDb.Create(FIREBASE_PROJID);
+            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", FIREBASE_PROJID);
+        }*/
+
+        //save data
+        public async System.Threading.Tasks.Task SaveProjects(Project project, Dictionary<string, object> value)
+        {
+            CollectionReference collectionRef = _firestoreDb.Collection("project");
+            DocumentReference docref = collectionRef.Document(project.Name.ToString());
+
+            Dictionary<string, object> values = new Dictionary<string, object> 
+            {
+                { "Project.Name", project.Name}
+            };
+
+            await docref.SetAsync(value);
+        }
+
+        //restore data
+        public async Task<Dictionary<string, object>> RestoreProjects(string project, string projectName)
+        {
+            DocumentReference docref = _firestoreDb.Collection("project").Document(projectName);
+            DocumentSnapshot snapshot = await docref.GetSnapshotAsync();
+
+            if (snapshot.Exists)
+            {
+                return snapshot.ToDictionary();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ .NET/TeamList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class TeamList
+    {
+        public List<Team> Teams { get; set; }
+
+        public TeamList()
+        {
+            Teams = new List<Team>();
+        }
+
+        public void AddTeam(Team team)
+        {
+            Teams.Add(team);
+        }
+
+        public void RemoveTeam(Team team)
+        {
+            Teams.Remove(team);
+        }
+
+        public List<Team> GetTeams()
+        {
+            return Teams;
+        }
+
+        public Team FindTeam(string teamName)
+        {
+            return Teams.FirstOrDefault(t => t.Name == teamName);
+        }
+    }
+}
