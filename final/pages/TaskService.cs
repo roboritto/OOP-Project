70b66d211363becafe29648231a88f432af714ff
hritthh <harithmahathir26@gmail.com>
2024-11-22 10:57:15 +0800

    Add files via upload

---
+++ final/pages/TaskService.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoDotnet8BlazorQuickGrid.Pages
+{
+    public class TaskService
+    {
+        public Dictionary<int, List<TaskDetail>> ProjectTasks { get; set; } = new();
+
+        public List<TaskDetail> GetTasks(int projectId)
+        {
+            if (!ProjectTasks.ContainsKey(projectId))
+            {
+                ProjectTasks[projectId] = new List<TaskDetail>();
+            }
+            return ProjectTasks[projectId];
+        }
+    }
+
+    public class TaskDetail
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TeamsAssigned { get; set; }
+        public string Progress { get; set; }
+        public bool IsEditing { get; set; } = false;
+    }
+
+}
---
+++ final/pages/TeamMemberService.cs
@@ -0,0 +1,46 @@
+﻿namespace DemoDotnet8BlazorQuickGrid.Pages
+{
+    public class TeamMemberService
+    {
+        // Dictionary to store team members (team name as key, list of members as value)
+        public Dictionary<string, List<string>> TeamMembers { get; private set; } = new Dictionary<string, List<string>>();
+
+        // Add a team member
+        public void AddMember(string teamName, string memberName)
+        {
+            if (!TeamMembers.ContainsKey(teamName))
+            {
+                TeamMembers[teamName] = new List<string>();
+            }
+            TeamMembers[teamName].Add(memberName);
+        }
+
+        // Edit a team member
+        public void EditMember(string teamName, string oldMemberName, string newMemberName)
+        {
+            if (TeamMembers.ContainsKey(teamName))
+            {
+                var members = TeamMembers[teamName];
+                var index = members.IndexOf(oldMemberName);
+                if (index >= 0)
+                {
+                    members[index] = newMemberName;
+                }
+            }
+        }
+
+        // Delete a team member
+        public void DeleteMember(string teamName, string memberName)
+        {
+            if (TeamMembers.ContainsKey(teamName))
+            {
+                TeamMembers[teamName].Remove(memberName);
+                if (TeamMembers[teamName].Count == 0)
+                {
+                    TeamMembers.Remove(teamName);
+                }
+            }
+        }
+    }
+
+}
